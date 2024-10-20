029c6b379477266ad3fd67e83987e234b514872e
Riccardo Deidda <riccardodeidda25@gmail.com>
2024-10-20 16:18:46 +0200

    split data function

--- src/fun_utils.py
+++ src/fun_utils.py
@@ -1,11 +1,18 @@
-from pandas import read_csv
 import numpy as np
+from pandas import read_csv
+
+def load_data(filename):
+    data = read_csv(filename)
+    z = np.array(data)
+    y = z[:, 0]
+    x = z[:, 1:]
+    return x, y
 
 
-def split_data(x, y, tr_frac=0.6):
+def split_data(x, y, tr_fraction=0.5):
     n_samples = x.shape[0]
 
-    n_tr = int(tr_frac * n_samples)
+    n_tr = int(tr_fraction * n_samples)
     n_ts = n_samples - n_tr
 
     idx = np.linspace(0, n_samples, num=n_samples, endpoint=False, dtype='int')
@@ -20,4 +27,4 @@ def split_data(x, y, tr_frac=0.6):
     ytr = y[tr_idx]
     xts = x[ts_idx, :]
     yts = y[ts_idx]
-    return xtr, ytr, xts, yts,
+    return xts, yts
